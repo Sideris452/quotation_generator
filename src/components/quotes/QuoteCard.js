954b73361599b6cb1fd14733ea2b70b5dd1ab8e5
D <167261788+Sideris452@users.noreply.github.com>
2024-09-27 14:30:15 -0700

    added card categories

--- src/components/quotes/QuoteCard.js
+++ src/components/quotes/QuoteCard.js
@@ -2,8 +2,15 @@ import React from "react";
 
 function QuoteCard({ quote }) {
   return (
-    <article className='quote-card'>
+    <article className={"quote-card"}>
       <div>
+        <p className='categories'>
+          {quote.categories.map((category) => (
+            <span className='category' key={category}>
+              {category}
+            </span>
+          ))}
+        </p>
         <h3>{quote.text}</h3>
       </div>
       <footer>
