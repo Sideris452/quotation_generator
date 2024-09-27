2e5a31d7831297e4f280109149d41ea64cee415b
D <167261788+Sideris452@users.noreply.github.com>
2024-09-27 14:55:42 -0700

    added a message for added favorites function component.

--- src/App.js
+++ src/App.js
@@ -4,12 +4,15 @@ import Footer from "./components/Footer";
 import { Loader } from "react-feather";
 import FavoriteQuotes from "./components/quotes/FavoriteQuotes";
 import Quotes from "./components/quotes/Quotes";
+import Message from "./components/Message";
 import "./App.css";
 
 function App() {
   const [quotes, setQuotes] = useState([]);
   const [loading, setLoading] = useState(false);
   const [category, setCategory] = useState("All");
+  const [showMessage, setShowMessage] = useState(false);
+  const [messageText, setMessageText] = useState("");
   const [favoriteQuotes, setFavoriteQuotes] = useState([]);
   const quotesUrl =
     "https://gist.githubusercontent.com/skillcrush-curriculum/6365d193df80174943f6664c7c6dbadf/raw/1f1e06df2f4fc3c2ef4c30a3a4010149f270c0e0/quotes.js";
@@ -47,10 +50,12 @@ function App() {
       removeFromFavorites(quoteId);
     } else {
       if (favoriteQuotes.length < maxFaves) {
-        console.log("Added to Favorites!");
+        setMessageText("Added to Favorites!");
+        setShowMessage(true);
         setFavoriteQuotes([...favoriteQuotes, selectedQuote]);
       } else {
-        console.log("Max number of favorite quotes reached. Remove one to add another.");
+        setMessageText("Max number of favorite quotes reached. Remove one to add another.");
+        setShowMessage(true);
       }
     }
   };
@@ -60,8 +65,13 @@ function App() {
     setFavoriteQuotes(updatedFavorites);
   };
 
+  const removeMessage = () => {
+    setShowMessage(false);
+  };
+
   return (
     <div className='App'>
+      {showMessage && <Message messageText={messageText} removeMessage={removeMessage} />}
       <Header />
       <main>
         <FavoriteQuotes favoriteQuotes={favoriteQuotes} maxFaves={maxFaves} removeFromFavorites={removeFromFavorites} />
